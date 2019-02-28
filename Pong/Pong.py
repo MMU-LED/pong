9f79f8770ad94d1e12455fdd9459005d2d8c1a18
AfraidBob <AfraidBob1@gmail.com>
2019-02-28 13:34:27 +0000

    Begin Refactor of pong.py

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -1,16 +1,3 @@
-#main - so far this is just a placeholder 
-#the paddle and ball classes need to be used in here
-#background needs creating
-#message needs adding
-#etc etc
-#remember python does nesting by indentation, not by curly brackets :) 
-#~josh 21/01/19
-
-#created a main
-#updated the ball class to add collisions wth border of screen and winning conditions when hit either side of screen
-#created paddle class with player input ( up/down arrows) and collide with ball
-#-Aaron 30/01/2019
-
 from Ball import Ball
 
 from Paddle import Paddle
@@ -21,40 +8,52 @@ import pygame, sys
 from pygame.locals import *
 #useful imports - not fully sure what they're for though
 
+player1_points = 0
+player2_points = 0
+p1Control = False   #represents wether paddle is controlled by a player
+p2Control = False
+screensize = (0,0)
+player1_paddle = None
+player2_paddle = None
+ball = None
+screen = None
+
+# Read only
+scale = 6 # 1 for led screen, 6-ish for testing
+height = 80*scale
+width = 96*scale 
 
 def main():
+    global player1_points
+    global player2_points
+    global screensize
+    global p1Control
+    global p2Control
+    global ball
+    global screen
+    global player1_paddle
+    global player2_paddle
+
+
+    # Initialise PyGame and create screen object
     pygame.init() 
     pygame.font.init()
-    #display
-    scale = 6#1 for led screen, 6-ish for testing
-    height = 80*scale
-    width = 96*scale 
-    player1_points = 0
-    player2_points = 0
     screensize = (height, width)
-    p1Control = False   #represents wether paddle is controlled by a player
-    p2Control = False
     myFont = pygame.font.SysFont("monospace", 10*scale)
-
-  
-    #create a new 'screen'/drawing surface - 300x300 resolution 
-    #in pygame you have to create a 'surface' and then draw stuff on that surface similar to processing
-    #you can have multiple surfaces on screen at once I think - think different menus in a game
     screen = pygame.display.set_mode((screensize))
+    pygame.display.set_caption('MMUARCADE2018')
    
-    #limit and track FPS
+    # limit and track FPS
     clock = pygame.time.Clock()
     
+    # Create Ball and paddles
     ball = Ball(screensize, scale)
-    player_paddle = Paddle(screensize, screensize[0]-(2*scale), scale)
-    #player_paddle.color = 255,100,100 # red player             TO BE REMOVED
+    player1_paddle = Paddle(screensize, screensize[0]-(2*scale), scale)
     player2_paddle = Paddle(screensize, 2*scale, scale)
 
-    # Window Title
-    pygame.display.set_caption('MMUARCADE2018')
-  
     #this is like void draw 
     #loop below forever
+    
     running = True 
     while running: 
         
@@ -72,18 +71,18 @@ def main():
             if event.type == KEYDOWN:
                 if event.key == K_UP:
                     p1Control = True    # p1 takes control
-                    player_paddle.color = 255,100,100 # paddle turns red
-                    player_paddle.direction = -1
+                    player1_paddle.color = 255,100,100 # paddle turns red
+                    player1_paddle.direction = -1
                 elif event.key == K_DOWN:
                     p1Control = True    # p1 takes control
-                    player_paddle.color = 255,100,100 # paddle turns red
-                    player_paddle.direction = 1
+                    player1_paddle.color = 255,100,100 # paddle turns red
+                    player1_paddle.direction = 1
             # when no button is pressed , paddle stops moving
             if event.type == KEYUP:
-                if event.key == K_UP and player_paddle.direction == -1:
-                    player_paddle.direction = 0
-                elif event.key == K_DOWN and player_paddle.direction == 1:
-                    player_paddle.direction = 0
+                if event.key == K_UP and player1_paddle.direction == -1:
+                    player1_paddle.direction = 0
+                elif event.key == K_DOWN and player1_paddle.direction == 1:
+                    player1_paddle.direction = 0
 
             # keyboard input for paddle 2 
             #SACRIFICIAL FEATURE - A way for paddles switch to AI without game ending?
@@ -106,9 +105,9 @@ def main():
               
 #####object updating phase
         
-        player_paddle.update()
+        player1_paddle.update()
         player2_paddle.update()
-        ball.update(player_paddle, player2_paddle, scale)
+        ball.update(player1_paddle, player2_paddle, scale)
 
         # very basic singleplayer - p2 moves on own until input takes over
         if p2Control == False: #ai below here
@@ -118,49 +117,17 @@ def main():
                 player2_paddle.centery -= int (1*scale)/2
         
         if p1Control == False: #ai below here
-            if  ball.centery > player_paddle.centery:
-                player_paddle.centery += int (1*scale)/2
-            if  ball.centery < player_paddle.centery:
-                player_paddle.centery -= int (1*scale)/2
+            if  ball.centery > player1_paddle.centery:
+                player1_paddle.centery += int (1*scale)/2
+            if  ball.centery < player1_paddle.centery:
+                player1_paddle.centery -= int (1*scale)/2
 
 
         if ball.hit_edge_left:
-            print ('One point for Player 1!')
-            player1_points += 1
-            print (player1_points)
-            ball = None
-            player_paddle = None
-            player2_paddle = None
-            ball = Ball(screensize, scale)
-            player_paddle = Paddle(screensize, screensize[0]-(2*scale), scale)
-            player2_paddle = Paddle(screensize, 2*scale, scale)
-            if p1Control == True:
-                player_paddle.color = 255,100,100 # red player
-            if p2Control == True:
-                player2_paddle.color = 100,100,255 # blue player
-            if player1_points == 2:
-                print ('Player 1 wins!')
-                player1_points = 0
-                player2_points = 0
+            pointScored("left")
             #running = False
         elif ball.hit_edge_right:
-            print ('One point for Player 2!')
-            player2_points += 1
-            print (player2_points)
-            ball = None
-            player_paddle = None
-            player2_paddle = None
-            ball = Ball(screensize, scale)
-            player_paddle = Paddle(screensize, screensize[0]-(2*scale), scale)
-            player2_paddle = Paddle(screensize, 2*scale, scale)
-            if p1Control == True:
-                player_paddle.color = 255,100,100 # red player
-            if p2Control == True:
-                player2_paddle.color = 100,100,255 # blue player
-            if player2_points == 2:
-                print ('Player 2 wins')
-                player1_points = 0
-                player2_points = 0
+            pointScored("right")
             #running = False
 
 #####rendering phase
@@ -172,7 +139,7 @@ def main():
         #render text
         label = myFont.render(str(player2_points) ,True, (100,100,100))
         screen.blit(label, (320, 50))
-        player_paddle.render(screen, scale)
+        player1_paddle.render(screen, scale)
         player2_paddle.render(screen, scale)
         ball.render(screen, scale)
         #pygame uses 2 screens , so i think it renders 1 frame ahead ( not entirely sure )
@@ -180,4 +147,65 @@ def main():
         #update display - think void draw from processing
         pygame.display.update()
     pygame.quit()
+
+def keyboardHandler():
+    print("Placeholder")
+
+def paddleHandler():
+    print("Placeholder")
+
+def pointScored(side):
+    global player1_points
+    global player2_points
+    global screensize
+    global p1Control
+    global p2Control
+    global ball
+    global screen
+    global player1_paddle
+    global player2_paddle
+
+    if side == "left":
+        # ball has gone left
+        print("One point for Player 1!")
+        player1_points += 1
+        print (player1_points)
+        ball = None
+        player1_paddle = None
+        player2_paddle = None
+        ball = Ball(screensize, scale)
+        player1_paddle = Paddle(screensize, screensize[0]-(2*scale), scale)
+        player2_paddle = Paddle(screensize, 2*scale, scale)
+        if p1Control == True:
+            player1_paddle.color = 255,100,100 # red player
+        if p2Control == True:
+            player2_paddle.color = 100,100,255 # blue player
+        if player1_points == 2:
+            print ('Player 1 wins!')
+            player1_points = 0
+            player2_points = 0
+    elif side == "right":
+        # ball has gone right
+        print ('One point for Player 2!')
+        player2_points += 1
+        print (player2_points)
+        ball = None
+        player1_paddle = None
+        player2_paddle = None
+        ball = Ball(screensize, scale)
+        player1_paddle = Paddle(screensize, screensize[0]-(2*scale), scale)
+        player2_paddle = Paddle(screensize, 2*scale, scale)
+        if p1Control == True:
+            player1_paddle.color = 255,100,100 # red player
+        if p2Control == True:
+            player2_paddle.color = 100,100,255 # blue player
+        if player2_points == 2:
+            print ('Player 2 wins')
+            player1_points = 0
+            player2_points = 0
+
+
+def resetGame():
+    print("Game Reset")
+    # Reset Stuff
 main()
